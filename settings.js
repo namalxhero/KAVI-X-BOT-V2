ebd6500195f689e80b98ac1714e901b7af1672af
namalxhero <nipunanamal66@gmail.com>
2025-08-15 12:10:04 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-global.sessionID = process.env.SESSIONID || ""
+global.sessionID = process.env.SESSIONID || "~etQUxLiL#rGuUqbjvvPhzhacHD4nT9xNWUr9xB_rE9vU8UOdFz_U"
 global.mongodburi = process.env.MONGODB_URI || ""
-global.botname = process.env.BOTNAME || "KAVI-X MD"
+global.botname = process.env.BOTNAME || "NIPUNA-X MD"
 global.ownernumber = process.env.OWNERNUMBER || "94766577249"
 global.ownername = process.env.OWNERNAME || "Cyber Kavi"
 global.packname = process.env.PACKNAME || "KAVI-X MD"
